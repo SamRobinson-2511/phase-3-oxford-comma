b26fb2c011aded11ed2eb24d5b521cfbc2040c2b
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-12-18 00:50:27 -0600

    commit tree 211a80ce323643ab0e56c16075f82e186a0d6d36

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,35 @@
-def oxford_comma(array)
+require 'pry'
 
+
+
+# def oxford_comma(array)
+#     if array.length == 1
+#         return array.join
+#     elsif array.length == 2
+#         return array.join(" and ")
+#     elsif array.length >= 3
+#         x = array.pop()
+#         array.join(", and ")
+#         array << x
+#     end
+# end
+
+def oxford_comma(array)
+    last_element = array.pop
+    
+    if array.length == 0
+        last_element
+     elsif array.length == 1
+          array.join << " and " + last_element
+    else
+        array.join(", ") << ", and " + last_element
+    end
 end
+
+
+oxford_comma([])
+
+
+
+
+
